65ec3abdb3c60845f167472521047ce485a0fc87
FlysJoint <wattoo42@hotmail.com>
2020-04-19 23:31:16 +0100

    initial commit for all exercises, no additional tests

--- challenges/week3.js
+++ challenges/week3.js
@@ -21,19 +21,41 @@ return capitalised.join('');
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  let subjects = 0;
+
+  for (let i = 0; i < people.length; i++) {
+    subjects += people[i].subjects.length;
+  }
+  
+  return subjects;
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+
+  let subjects = 0;
+
+  for (let i = 0; i < menu.length; i++) {
+    if (menu[i].ingredients.includes(ingredient)) return true;
+  }
+  
+  return false;
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+
+  let duplicates = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j] && duplicates.indexOf(arr1[i]) === -1) duplicates.push(arr1[i]);
+    }
+  }
+  return duplicates.sort((a, b) => a - b);
 }
 
 module.exports = {
